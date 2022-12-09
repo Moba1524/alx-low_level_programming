9a2b98514b796f5b3c3a16a828d4ff5226474b8d
Mothafar <mothafr2016@gmail.com>
2022-12-08 17:03:13 -0800

    Task 4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  main - print a given phrase
+ *  main - prints a given phrase
  *  Return: 0 if exited properly, non-zero otherwise
  */
 
 int  main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle")
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
