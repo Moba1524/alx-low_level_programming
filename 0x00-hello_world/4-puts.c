5b3aff638355cf64214c218e6d744445ef043edd
Mothafar <mothafr2016@gmail.com>
2022-12-08 18:59:06 -0800

    Puts update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  *  main - prints a given phrase
  *  Return: 0 if exited properly, non-zero otherwise
  */
-
 int  main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
+        puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }	
