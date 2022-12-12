7ca493d10cecd2eefed23ed556a6bc87fd4b34ab
Mothafar <mothafr2016@gmail.com>
2022-12-11 18:24:11 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
- * - Dora korpar, 2015-10-19
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora korpar, 2015-10-19",
  * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
