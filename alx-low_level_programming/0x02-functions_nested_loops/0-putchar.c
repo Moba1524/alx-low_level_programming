96b7f6cf5cb296d5ff00f4b441e94cb6a4e8e2f6
Mothafar <mothafr2016@gmail.com>
2022-12-13 18:41:09 -0800

    Task 0 updated

--- alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
+++ alx-low_level_programming/0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
 #include <main.sh>
+/**
+ * main - Entry point 
+ *
+ * Return: 0 nothing, return void
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
