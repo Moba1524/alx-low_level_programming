0a158c6831adcf397c027c330d550d14b0a4dddf
Mothafar <mothafr2016@gmail.com>
2022-12-22 08:23:44 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates the string pointed to by @src to 
+ *_strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
  *@dest: string that will be appended
  *@src: string to be concatenated upon
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+*print_buffer - C function yhat prints the content of an
+*  inputted number of bytes from abuffer.
+* prints 10 bytes per line.
+* Starts with the position of the first byte in hexadecimal (8 chars)'
+* starting with '0'.
+* Each line shows the hexadcimal content (2 chars) of the buffer,
+*
