90500066c968c36ccaff678c816e43f03afa7a3a
Mothafar <mothafr2016@gmail.com>
2022-12-22 08:32:32 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,8 +3,8 @@
  *_strncat - concatenate two strings but add inputted number of bytes
  *@dest: string to be appended upon
  *@src: string to be completed at end of dest
- *@n: integer parameter to compare index to 
- *Return: returns new concatenated string 
+ *@n: integer parameter to compare index to
+ *Return: returns new concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
