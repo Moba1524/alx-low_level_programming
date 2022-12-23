dc9cb5f259b63c4cba80d351c8dd93a5cde48212
Mothafar <mothafr2016@gmail.com>
2022-12-23 04:15:33 -0800

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*cap_string - function that capitalize first chracter of a word.
-*@str: string to capitalize.
-*Return: returns the capitalized string.
+*cap_string - function that capitalize first character of a word
+*@str: string to capitalize
+*Return:returns the capitalized string
 */
 char *cap_string(char *str)
 {
