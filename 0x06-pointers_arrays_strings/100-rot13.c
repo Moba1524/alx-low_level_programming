591cc0c31dd2f738ac25c4f0d2cce5928922b3fd
Mothafar <mothafr2016@gmail.com>
2022-12-22 18:26:43 -0800

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,45 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - encodes a string using rot13
- * @str: the string targted
- * Return: returns the encoded string
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ * Return: *s
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int index1, index2;
-
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 
-		                                                                     'G', 'H', 'I', 'J', 'K', 'L',
-										     'M', 'N', 'O', 'P', 'Q', 'R',
-										     'S', 'T', 'U', 'V', 'W', 'X',
-										     'Y', 'Z', 'a', 'b', 'c', 'd',
-										     'e', 'f', 'g', 'h', 'i', 'j',
-										     'k', 'l', 'm', 'n', 'o', 'p',
-										     'q', 'r', 's', 't', 'u', 'v',
-										     'w', 'x', 'y', 'z'};
-	
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',                            
-                                                                                     'T', 'U', 'V', 'W', 'X', 'Y',
-								                     'Z', 'A', 'B', 'C', 'D', 'E',
-							                             'F', 'G', 'H', 'I', 'J', 'K',
-							                             'L', 'M', 'n', 'o', 'p', 'q',
-						                                     'r', 's', 't', 'u', 'v', 'w',
-							                             'x', 'y', 'z', 'a', 'b', 'c',
-							                             'd', 'e', 'f', 'g', 'h', 'i',
-							                             'j', 'k', 'l', 'm'};
-        while (str[++index1])
+	int i;
+	int j;
+	char data1[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	        for (index2 = 0; index2 < 52; index2++)
+		for (j = 0; j < 52; j++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (s[i] == data1[j])
 			{
-				str[index1] = rot13key[index2];
+				s[i] = datarot[j];
 				break;
 			}
 		}
 	}
-	return (str);
+	return (s);
 }
-
