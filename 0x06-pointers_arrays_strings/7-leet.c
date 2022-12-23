77ea37be5b2ab8d8ca27673c0636879ac624db8e
Mothafar <mothafr2016@gmail.com>
2022-12-22 17:54:50 -0800

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* leet - function thaty encode a string
+* leet - function that encode a string
 *@str:string that will be encoded
-*Return:returns encoded string 
+*Return:returns encoded string
 */
 
 char *leet(char *str)
