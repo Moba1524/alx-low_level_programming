d033dd334b440eff89aa813bb5b57a76ada15909
Mothafar Bashir <mothafr2016@gmail.com>
2023-02-06 16:15:47 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CRAT | O_RDWR | O_TRUNC, 0000);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
