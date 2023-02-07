a3491dfe5b218e6bc6509886002f1cc52ce1a4bc
Mothafar Bashir <mothafr2016@gmail.com>
2023-02-06 18:49:22 -0800

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -67,10 +67,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
 	do {
 		if (from == -1 || r == -1)
 		{
@@ -79,23 +81,24 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(98);
 		}
-		
+
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,
+			dprintf(STDERR_FILEND,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
-		
+
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
+
 	} while (r > 0);
-	
+
 	free(buffer);
 	close_file(from);
 	close_file(to);
-	
+
 	return (0);
 }
